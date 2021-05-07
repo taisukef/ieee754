b55119edabdc4298cb5618ca7be4670503cad945
Taisuke Fukuno <fukuno@jig.jp>
2021-05-08 06:05:40 +0900

    fix use Deno.test

--- test/test.js
+++ test/test.js
@@ -1,26 +1,24 @@
+import { assert, fail, assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { ieee754 } from "../ieee754.js";
-import { test } from "./tape-mini.js";
 
 const EPSILON = 0.00001
 
-test('read & write float', function (t) {
+Deno.test('read & write float', function () {
   const val = 42.42
   const buf = new Uint8Array(4);
   
   ieee754.write(buf, val, 0, true, 23, 4);
   const num = ieee754.read(buf, 0, true, 23, 4)
 
-  t.ok(Math.abs(num - val) < EPSILON)
-  t.end()
+  assert(Math.abs(num - val) < EPSILON)
 })
 
-test('read & write double', function (t) {
+Deno.test('read & write double', function () {
   const value = 12345.123456789
   const buf = new Uint8Array(8);
 
   ieee754.write(buf, value, 0, true, 52, 8)
   const num = ieee754.read(buf, 0, true, 52, 8)
 
-  t.ok(Math.abs(num - value) < EPSILON)
-  t.end()
+  assert(Math.abs(num - value) < EPSILON)
 })
